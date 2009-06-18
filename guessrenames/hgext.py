8efeecc6654d5335bd4cda7800cefc360f146427
Colin Barrett <colin@springsandstruts.com>
2009-06-18 12:50:46 -0700

    hgext: Improve the our appearance in the help docs dramatically. Further copyediting is welcome.

--- guessrenames/hgext.py
+++ guessrenames/hgext.py
@@ -6,6 +6,7 @@
 
 from hg import *
 from mercurial import extensions, commands, cmdutil
+import re
 
 def addremove(orig, repo, pats=[], opts={}, dry_run=None, similarity=None):
     shouldguess = False
@@ -27,16 +28,33 @@ def addremove(orig, repo, pats=[], opts={}, dry_run=None, similarity=None):
 
 
 def uisetup(ui):
+    def replace_similarity(opts):
+        newopts = []
+        for idx, opt in enumerate(opts):
+            if opt[0] == 's':
+                newopt = list(opt)
+                newopt[3] = 'backwards compatability; implies -g/--guess'
+                opt = tuple(newopt)
+            newopts.append(opt)
+        return newopts
+    
     ar = list(commands.table['addremove'])
-    opts = ar[1]
-    # XXX need to modify 's' to indicate it's deprecated and invokes -g instead.
-    opts.append(('g', 'guess', False, 'guess renamed files'))
+    docre = re.compile('Use the -s option.+way can be expensive\.', re.M | re.S)
+    ar[0].__doc__ = re.sub(docre,
+    """
+    Use the -g option to detect renamed files. This option uses a smarter, more
+    accurate algorithm than the built-in -s option.
+    """.strip(), ar[0].__doc__)
+    ar[1] = replace_similarity(ar[1])
+    ar[1].append(('g', 'guess', False, 'guess renamed files'))
     commands.table['addremove'] = tuple(ar)
     
     import_ = list(commands.table['import|patch'])
-    opts = import_[1]
-    # XXX need to modify 's' to indicate it's deprecated and invokes -g instead.
-    opts.append(('g', 'guess', False, 'guess renamed files'))
+    import_[0].__doc__ = re.sub(r'(--similarity),',
+                                r'\1 or -g/--guess,',
+                                import_[0].__doc__)
+    import_[1] = opts = replace_similarity(import_[1])
+    import_[1].append(('g', 'guess', False, 'guess renamed files'))
     commands.table['import'] = tuple(import_)
             
     extensions.wrapfunction(cmdutil, 'addremove', addremove)
